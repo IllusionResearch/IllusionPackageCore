26f19d480a4f6a8ea86668de017936477d09f5da
sawich <sawich@angeldev.org>
2022-01-27 22:40:11 +0200

    Initial D

---
+++ GameToken.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IllusionPackageCore;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum GameToken
+{
+    None = 0,
+    Koikatsu = 1,
+}
---
+++ WebPackage.cs
@@ -0,0 +1,3 @@
+﻿namespace IllusionPackageCore;
+
+internal sealed record WebPackage(string Url, Version Version);
